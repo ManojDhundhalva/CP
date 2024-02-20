17f1809d1cf78e86353169e4a06c8d521d5db10f
ManojDhundhalva <134536482+ManojDhundhalva@users.noreply.github.com>
2024-02-20 19:01:24 +0530

    Update segment_tree.cpp

--- Data_Structure/segment_tree.cpp
+++ Data_Structure/segment_tree.cpp
@@ -2,9 +2,7 @@
 #define ll long long int
 using namespace std;
 
-vector<ll> a(100005), seg(4 * 100005]);
-
-void build(ll ind, ll low, ll high)
+void build(ll ind, ll low, ll high, vector<ll> &a, vector<ll> &seg)
 {
 	if (low == high)
 	{
@@ -13,12 +11,12 @@ void build(ll ind, ll low, ll high)
 	}
 
 	ll mid = low + ((high - low) / 2);
-	build(2 * ind + 1, low, mid);
-	build(2 * ind + 2, mid + 1, high);
+	build(2 * ind + 1, low, mid, a, seg);
+	build(2 * ind + 2, mid + 1, high, a, seg);
 	seg[ind] = max(seg[2 * ind + 1], seg[2 * ind + 2]);
 }
 
-ll query(ll ind, ll low, ll high, ll l, ll r)
+ll query(ll ind, ll low, ll high, ll l, ll r, vector<ll> &seg)
 {
 	if (low >= l && high <= r)
 	{
@@ -27,23 +25,24 @@ ll query(ll ind, ll low, ll high, ll l, ll r)
 	if (high < l || low > r) { return INT_MIN; }
 
 	ll mid = low + ((high - low) / 2);
-	ll left = query(2 * ind + 1, low, mid, l, r);
-	ll right = query(2 * ind + 2, mid + 1, high, l, r);
+	ll left = query(2 * ind + 1, low, mid, l, r, seg);
+	ll right = query(2 * ind + 2, mid + 1, high, l, r, seg);
 	return max(left, right);
 }
 
-
-int main()
+void code()
 {
+	// Question : Find max element in the range [L, R]
+
 	ll n;
 	cin >> n;
 
+	vector<ll> a(n), seg(4 * n);
 	for (ll i = 0; i < n; ++i)
 	{
 		cin >> a[i];
 	}
-
-	build(0, 0, n - 1);
+	build(0, 0, n - 1, a, seg);
 
 	ll q;
 	cin >> q;
@@ -53,8 +52,29 @@ int main()
 		ll l, r;
 		cin >> l >> r;
 
-		cout << query(0, 0, n - 1, l, r) << '\n';
-	}
+		l--;
+		r--;
 
+		cout << query(0, 0, n - 1, l, r, seg) << '\n';
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ll t;
+	cin >> t;
+
+	while (t--)
+	{
+		code();
+	}
 	return 0;
 }
